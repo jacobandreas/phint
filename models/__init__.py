0fb02a95f5d152235e5682c5a97c2ad6c76c096d
Jacob <jdandreas@gmail.com>
2016-12-05 08:37:26 +0100

    disable some models

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
-from manual import ManualModel
-from simple_ac import SimpleACModel
-from simple_q import SimpleQModel
-from recurrent_q import RecurrentQModel
-from memory import MemoryModel
+#from manual import ManualModel
+#from simple_ac import SimpleACModel
+#from simple_q import SimpleQModel
+#from recurrent_q import RecurrentQModel
+#from memory import MemoryModel
 from modular import ModularModel
 
 def load(config, world):
