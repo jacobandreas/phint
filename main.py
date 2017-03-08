d062b6408e6fcfaf8c4521fafa26d8ce5d687d39
Jacob <jdandreas@gmail.com>
2017-03-07 20:34:24 -0800

    .

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def main():
     world = worlds.load(config)
     guide = guides.load(config)
     model = models.load(config, world, guide)
-    #objective = Ppo(config, model)
-    objective = Reinforce(config, model)
+    objective = Ppo(config, model)
     trainer = trainers.load(config)
     trainer.train(world, model, objective)
 
