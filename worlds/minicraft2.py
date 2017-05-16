2d4c8ffcffe2f1883425591f801be42df92d040e
Jacob <jdandreas@gmail.com>
2017-05-16 11:23:43 -0700

    short+few

--- worlds/minicraft2.py
+++ worlds/minicraft2.py
@@ -8,12 +8,15 @@ from skimage.measure import block_reduce
 SIZE = 7
 WINDOW_SIZE = 5
 
-WOOD_VARIANTS = ["oak", "pine", "birch"]
-ORE_VARIANTS = ["copper", "iron", "nickel"]
-STONE_VARIANTS = ["granite", "quartz", "slate"]
+#WOOD_VARIANTS = ["oak", "pine", "birch"]
+#ORE_VARIANTS = ["copper", "iron", "nickel"]
+#STONE_VARIANTS = ["granite", "quartz", "slate"]
 #WOOD_VARIANTS = ["oak", "pine"]
 #ORE_VARIANTS = ["copper", "iron"]
 #STONE_VARIANTS = ["granite", "quartz"]
+WOOD_VARIANTS = ["oak"]
+ORE_VARIANTS = ["copper"]
+STONE_VARIANTS = ["granite"]
 
 INGREDIENTS = (
     ["wood_%s" % v for v in WOOD_VARIANTS]
